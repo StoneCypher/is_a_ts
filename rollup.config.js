a78efdfc2593d690c3d6a942167d3d08af707a9e
John Haugeland <stonecypher@gmail.com>
2021-07-24 11:09:07 -0700

    Working rollup, fixes #9

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+
+const es6_config = {
+
+  input   : 'build/typescript/index.js',
+
+  output  : {
+    file   : 'build/rollup/index.js',
+    format : 'es',
+    name   : 'is_a_ts'
+  }
+
+};
+
+
+
+
+
+export default [ es6_config ];
