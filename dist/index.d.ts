37d30637d62b25e410c27bb972580d52259903ea
John Haugeland <stonecypher@gmail.com>
2021-07-24 13:20:58 -0700

    implement minification, fixes #10, fixes #23.  split rollup to esm, cjs, fixes #22.  sets types and module, fixes #21, fixes #20.

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { version, built, packagename } from './generated_code/package_version';
+import { typeMap, PrimitiveOrConstructor, GuardedType, typeGuard } from './is_a';
+export { version, built, packagename, typeMap, PrimitiveOrConstructor, GuardedType, typeGuard };
--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ const es6_config = {
   input   : 'build/typescript/index.js',
 
   output  : {
-    file   : 'build/rollup/index.js',
+    file   : 'build/rollup/index.esm.js',
     format : 'es',
     name   : 'is_a_ts'
   },
@@ -34,4 +34,33 @@ const es6_config = {
 
 
 
-export default [ es6_config ];
+const cjs_config = {
+
+  input   : 'build/typescript/index.js',
+
+  output  : {
+    file    : 'build/rollup/index.cjs.js',
+    format  : 'commonjs',
+    name    : 'is_a_ts',
+    exports : 'auto'
+  },
+
+  plugins : [
+
+    commonjs(),
+
+    nodeResolve({
+      mainFields     : ['module', 'main'],
+      extensions     : [ '.js' ],
+      preferBuiltins : false
+    })
+
+  ]
+
+};
+
+
+
+
+
+export default [ es6_config, cjs_config ];
